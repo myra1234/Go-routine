ee00b9ddeb892cfacc787552b713653a97ac9eb3
myra1234 <34545961+myra1234@users.noreply.github.com>
2021-08-11 00:14:11 -0500

    Create main.go

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+
+	go func() {
+		count("bird")
+		wg.Done()
+	}()
+
+	wg.Wait()
+}
+
+func count(thing string) {
+	for i := 1; i <= 5; i++ {
+		fmt.Println(i, thing)
+		time.Sleep(time.Millisecond * 500)
+	}
+}
